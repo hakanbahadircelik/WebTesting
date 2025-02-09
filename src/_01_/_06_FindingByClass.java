b6ebfaf35986d7fd42bd331911f78ee285b3d60a
Baha <hakanbahadircelik@gmail.com>
2025-02-09 16:40:31 +0400

    finding with class

---
+++ src/_01_/_06_FindingByClass.java
@@ -0,0 +1,24 @@
+package _01_;
+
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _06_FindingByClass {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353");
+
+        WebElement textBox1 = driver.findElement(By.className("form-textbox"));
+        // if there is more than one same element in findElement, it sends first one
+        textBox1.sendKeys("baha");
+
+
+        MyFunc.Wait(3);
+        driver.quit();
+
+    }
+}
