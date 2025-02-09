bccc9b573d34951cfe173a7dec6eb6c1acf4fd14
Baha <hakanbahadircelik@gmail.com>
2025-02-09 15:59:41 +0400

    Initial commit

---
+++ src/_01_/_01_OpeningWebsites.java
@@ -0,0 +1,19 @@
+package _01_;
+
+import Utility.MyFunc;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OpeningWebsites {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.hepsiburada.com/");
+
+
+        MyFunc.Wait(3);
+//        driver.close(); // only close this one
+        driver.quit(); // deletinf all of them
+    }
+}
