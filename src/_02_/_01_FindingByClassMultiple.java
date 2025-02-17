de3133dc53f7085eb20e55de1d1cd584ba6e5d05
Baha <hakanbahadircelik@gmail.com>
2025-02-17 16:05:29 +0400

    with multiple finding

---
+++ src/_02_/_01_FindingByClassMultiple.java
@@ -0,0 +1,26 @@
+package _02_;
+
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class _01_FindingByClassMultiple {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hepsiburada.com/");
+
+        List<WebElement> labels = driver.findElements(By.className("i9jTSpEeoI29_M1mOKct"));
+        for (WebElement w : labels)
+            System.out.println("w.getText() = " + w.getText());
+
+
+        MyFunc.Wait(3);
+        driver.quit();
+
+    }
+}
