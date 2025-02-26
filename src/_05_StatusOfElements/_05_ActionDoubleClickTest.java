aa620419a019ff7e24108e702288c42ce08b5e34
Baha <hakanbahadircelik@gmail.com>
2025-02-26 10:47:15 +0400

    double and right clicks

---
+++ src/_05_StatusOfElements/_05_ActionDoubleClickTest.java
@@ -0,0 +1,29 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class _05_ActionDoubleClickTest extends BaseDriver {
+
+    @Test
+    public void Test01() {
+
+        driver.get("https://demoqa.com/buttons");
+
+        WebElement doubleClick = driver.findElement(By.xpath("//button[@id='doubleClickBtn']"));
+        MyFunc.Wait(1);
+
+        Actions actions = new Actions(driver); // web page open for actions
+        Action action = actions.moveToElement(doubleClick).doubleClick().build();
+        action.perform();
+
+
+        WaitAndQuit();
+    }
+
+}
---
+++ src/_05_StatusOfElements/_06_ActionRightClickTest.java
@@ -0,0 +1,30 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class _06_ActionRightClickTest extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("https://demoqa.com/buttons");
+
+        WebElement btnRightClick;
+        btnRightClick = driver.findElement(By.id("rightClickBtn"));
+        MyFunc.Wait(1);
+
+        Actions actions = new Actions(driver);
+        Action action = actions.contextClick(btnRightClick).build();
+        action.perform();
+
+
+        WaitAndQuit();
+    }
+
+}
