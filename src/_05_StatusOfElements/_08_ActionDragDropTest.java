5fc5c9cef7cff565ba123c79ecf9b06ca7801f0d
Baha <hakanbahadircelik@gmail.com>
2025-02-26 10:57:46 +0400

    drag something and drop anywhere

---
+++ src/_05_StatusOfElements/_08_ActionDragDropTest.java
@@ -0,0 +1,31 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _08_ActionDragDropTest extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+        Actions actions = new Actions(driver);
+
+        MyFunc.Wait(1);
+        WebElement oslo = driver.findElement(By.id("box1"));
+        WebElement norway = driver.findElement(By.id("box101"));
+
+        // 1. way
+        actions.dragAndDrop(oslo, norway).build().perform();
+
+        // 2. way
+        actions.clickAndHold(oslo).build().perform();
+        actions.moveToElement(norway).release().build().perform();
+
+        WaitAndQuit();
+    }
+}
