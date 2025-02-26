6e2a72e16f0cc8efa865cf611ebc9471c056ab6e
Baha <hakanbahadircelik@gmail.com>
2025-02-26 09:25:02 +0400

    finding element status on the page

---
+++ src/_05_StatusOfElements/_01_ElementStatus.java
@@ -0,0 +1,38 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _01_ElementStatus extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox");
+
+        WebElement tuesday = driver.findElement(By.id("gwt-debug-cwCheckBox-Tuesday-input"));
+
+        System.out.println("tuesday.isDisplayed() = " + tuesday.isDisplayed());
+        System.out.println("tuesday.isEnabled() = " + tuesday.isEnabled());
+        System.out.println("tuesday.isSelected() = " + tuesday.isSelected());
+        MyFunc.Wait(1);
+        tuesday.click();
+        MyFunc.Wait(1);
+        System.out.println("tuesday.isSelected() = " + tuesday.isSelected());
+
+
+        WebElement saturday = driver.findElement(By.id("gwt-debug-cwCheckBox-Saturday-input"));
+        MyFunc.Wait(1);
+        System.out.println("saturday.isDisplayed() = " + saturday.isDisplayed());
+        System.out.println("saturday.isEnabled() = " + saturday.isEnabled());
+        System.out.println("saturday.isSelected() = " + saturday.isSelected());
+        MyFunc.Wait(1);
+
+
+        WaitAndQuit();
+    }
+
+}
