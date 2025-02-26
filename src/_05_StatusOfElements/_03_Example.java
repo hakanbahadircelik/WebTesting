7913f182b7bb291239f7c2850fe35d2e0da507c2
Baha <hakanbahadircelik@gmail.com>
2025-02-26 10:29:42 +0400

    action click tested and small example

---
+++ src/_05_StatusOfElements/_03_Example.java
@@ -0,0 +1,71 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class _03_Example extends BaseDriver {
+
+    @Test
+    public void Test01() {
+
+        driver.get("https://www.facebook.com/");
+
+        // cookies' fix
+//        MyFunc.Wait(1);
+//        List<WebElement> cookiesAccept = driver.findElements(By.xpath("//button[text()='Allow all cookies']"));
+//        if (cookiesAccept.size() > 0)
+//            cookiesAccept.get(0).click();
+
+        WebElement createNewAcc = driver.findElement(By.xpath("//div[@class='_6ltg']/a"));
+        createNewAcc.click();
+        MyFunc.Wait(1);
+
+        WebElement name = driver.findElement(By.xpath("//input[@name='firstname']"));
+        name.sendKeys("baha");
+        MyFunc.Wait(1);
+
+        WebElement Sname = driver.findElement(By.xpath("//input[@name='lastname']"));
+        Sname.sendKeys("steel");
+        MyFunc.Wait(1);
+
+        // checking reEmail showing up or not
+        WebElement reEmail = driver.findElement(By.name("reg_email_confirmation__"));
+        Assert.assertFalse("reEmail showed up = ", reEmail.isDisplayed());
+        MyFunc.Wait(1);
+
+        WebElement email = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+        email.sendKeys("bahasteel@gmail.com");
+        MyFunc.Wait(1);
+
+        // checking reEmail showing up or not
+        Assert.assertTrue("reEmail is not showed up = ", reEmail.isDisplayed());
+        MyFunc.Wait(1);
+
+        // Selecting Birthday with SelectClass
+        WebElement month = driver.findElement(By.xpath("//select[@id='month']"));
+        Select mSelect = new Select(month);
+        mSelect.selectByVisibleText("Dec");
+        MyFunc.Wait(1);
+
+        WebElement day = driver.findElement(By.xpath("//select[@id='day']"));
+        Select dSelect = new Select(day);
+        dSelect.selectByVisibleText("15");
+        MyFunc.Wait(1);
+
+        WebElement year = driver.findElement(By.xpath("//select[@id='year']"));
+        Select ySelect = new Select(year);
+        ySelect.selectByVisibleText("2002");
+        MyFunc.Wait(1);
+
+
+        WaitAndQuit();
+    }
+
+}
---
+++ src/_05_StatusOfElements/_04_ActionClickTest.java
@@ -0,0 +1,35 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class _04_ActionClickTest extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("https://demoqa.com/buttons");
+
+        WebElement element = driver.findElement(By.xpath("//button[text()='Click Me']"));
+
+        Actions actions = new Actions(driver); // web page open for actions
+
+        Action action = actions.moveToElement(element).click().build();
+        // go to the element and wait there, prepare for action
+        System.out.println("Action got ready");
+
+        MyFunc.Wait(2);
+        action.perform(); // action starts
+        System.out.println("Action happend");
+
+        // short version
+        new Actions(driver).moveToElement(element).click().build().perform();
+
+        WaitAndQuit();
+    }
+}
