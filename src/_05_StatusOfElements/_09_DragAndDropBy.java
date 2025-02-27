0e4400c550f3084f6dcf9e231f7b02327fe1120b
Baha <hakanbahadircelik@gmail.com>
2025-02-27 11:31:22 +0400

    sliding something, using  with Action in the page

---
+++ src/_05_StatusOfElements/_09_DragAndDropBy.java
@@ -0,0 +1,42 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class _09_DragAndDropBy extends BaseDriver {
+
+    @Test
+    public void Test01() {
+
+        driver.get("https://talkerscode.com/webtricks/demo/demo_price-range-slider-using-jquery-css-and-php.php");
+        Actions actions = new Actions(driver);
+
+        WebElement leftHolder = driver.findElement(By.xpath("//div[@id='slider-range']/span[1]"));
+        MyFunc.Wait(1);
+
+        Action action = actions.dragAndDropBy(leftHolder, 100, 0).build();
+        action.perform();
+
+        WaitAndQuit();
+    }
+
+    @Test
+    public void Test02() {
+
+
+        WaitAndQuit();
+    }
+
+    @Test
+    public void Test03() {
+
+
+        WaitAndQuit();
+    }
+
+}
