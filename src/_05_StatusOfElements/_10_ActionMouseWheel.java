5e8b7c78ebf9cb483d807f5c20a26ffa74f25f32
Baha <hakanbahadircelik@gmail.com>
2025-02-27 12:02:45 +0400

    learned how to use scroll

---
+++ src/_05_StatusOfElements/_10_ActionMouseWheel.java
@@ -0,0 +1,48 @@
+package _05_StatusOfElements;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _10_ActionMouseWheel extends BaseDriver {
+
+    @Test
+    public void Test01(){  // scroll to element
+
+        driver.get("http://selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+        MyFunc.Wait(1);
+        WebElement iframe = driver.findElement(By.name("nested_scrolling_frame"));
+
+        new Actions(driver).scrollToElement(iframe).build().perform();
+
+        WaitAndQuit();
+    }
+
+    @Test
+    public void Test02(){  // scroll a little bit
+
+        driver.get("http://selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+        MyFunc.Wait(1);
+        WebElement iframe = driver.findElement(By.name("nested_scrolling_frame"));
+
+        new Actions(driver).scrollByAmount(0,1000).build().perform();
+
+        WaitAndQuit();
+    }
+
+//    @Test
+//    public void Test03(){  // step by step scroll till an element
+//
+//        driver.get("http://selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+//        MyFunc.Wait(1);
+//        WebElement iframe = driver.findElement(By.name("nested_scrolling_frame"));
+//
+//        new Actions(driver).scrollFromOrigin().build().perform();
+//
+//        WaitAndQuit();
+//    }
+
+}
