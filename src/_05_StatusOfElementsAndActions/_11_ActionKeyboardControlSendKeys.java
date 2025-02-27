bb11d22afa2bf87e161a1ac927715ba19030e561
Baha <hakanbahadircelik@gmail.com>
2025-02-27 12:24:10 +0400

    keyboard buttons using

---
+++ src/_05_StatusOfElementsAndActions/_11_ActionKeyboardControlSendKeys.java
@@ -0,0 +1,44 @@
+package _05_StatusOfElementsAndActions;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _11_ActionKeyboardControlSendKeys extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("http://facebook.com/");
+
+        WebElement txtBox = driver.findElement(By.id("email"));
+        MyFunc.Wait(1);
+
+        new Actions(driver).moveToElement(txtBox).click().keyDown(Keys.SHIFT).sendKeys("b").keyUp(Keys.SHIFT).sendKeys("aha").build().perform();
+
+        WaitAndQuit();
+    }
+
+    @Test
+    public void Test02(){
+
+        driver.get("https://demoqa.com/auto-complete");
+
+        WebElement txtBox = driver.findElement(By.xpath("//div[@class='auto-complete__value-container css-1hwfws3']"));
+        MyFunc.Wait(1);
+
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(txtBox).click().sendKeys("b").build().perform();
+        MyFunc.Wait(2);
+        actions.keyDown(Keys.ARROW_DOWN).keyDown(Keys.ENTER).build().perform();
+
+
+        WaitAndQuit();
+    }
+
+}
