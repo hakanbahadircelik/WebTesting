f990f67d54e815c7f82c21664a35fc922ed32558
Baha <hakanbahadircelik@gmail.com>
2025-02-28 09:36:03 +0400

    wrote WebDriverWait to the BaseDriver for using everytime on a new tests also

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -2,6 +2,7 @@ package Utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
@@ -12,12 +13,14 @@ public class BaseDriver {
 
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(BaseDriver.class);
     public static WebDriver driver;
+    public static WebDriverWait wait;
 
     static  {  //  if only static is there, its automatically starts at the top of the code page.
 
         Logger logger = Logger.getLogger(""); // get output logs
         logger.setLevel(Level.SEVERE); // show only errors
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // makes fullScreen
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // loading page wait till 30sec, or ERROR
