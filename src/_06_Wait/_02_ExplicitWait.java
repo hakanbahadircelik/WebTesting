3053d27a6720c25b7b40bf9fe1f8d06a67465bed
Baha <hakanbahadircelik@gmail.com>
2025-02-28 09:21:16 +0400

    small change and other detailed version of explicit

--- src/_06_Wait/_02_ExplicitWait.java
+++ src/_06_Wait/_02_ExplicitWait.java
@@ -15,16 +15,18 @@ import java.time.Duration;
 public class _02_ExplicitWait extends BaseDriver {
 
     @Test
-    public void Test01(){
+    public void Test01() {
 
         driver.get("https://seleniumpractise.blogspot.com/2016/08/how-to-use-explicit-wait-in-selenium.html");
 
         WebElement button = driver.findElement(By.xpath("//button[@onclick='timedText()']"));
         button.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); //  wait till you find 20sec
-        wait.until(ExpectedConditions.textToBe(By.id("demo"),"WebDriver")); // wait an element of 'demo', till
-                                                                                  // message of 'WebDriver' comes up
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); //  wait till you find 20sec (only 1 time writing this)
+        wait.until(ExpectedConditions.textToBe(By.id("demo"), "WebDriver")); // wait an element of 'demo', till
+                                                                                    // message of 'WebDriver' comes up
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//button[@onclick='timedText()']")));
+        // wait till this element visible
 
         WebElement message = driver.findElement(By.id("demo"));
         System.out.println("message = " + message.getText());
