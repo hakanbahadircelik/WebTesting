df4b9548c6a58afc74c53080b327cd7849b7d2d7
Baha <hakanbahadircelik@gmail.com>
2025-02-28 09:29:43 +0400

    there was a error about chrome and selenium connection. not error but its just shows up everytime. so from now its not showing up anymore.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -2,13 +2,22 @@ package Utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.LoggerFactory;
+
 import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BaseDriver {
 
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(BaseDriver.class);
     public static WebDriver driver;
 
     static  {  //  if only static is there, its automatically starts at the top of the code page.
+
+        Logger logger = Logger.getLogger(""); // get output logs
+        logger.setLevel(Level.SEVERE); // show only errors
+
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // makes fullScreen
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // loading page wait till 30sec, or ERROR
