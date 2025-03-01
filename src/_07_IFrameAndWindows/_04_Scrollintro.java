85ded558b265c3a02dbb0bf93faa9a4fd51fe618
Baha <hakanbahadircelik@gmail.com>
2025-03-01 14:30:06 +0400

    with java script

---
+++ src/_07_IFrameAndWindows/_04_Scrollintro.java
@@ -0,0 +1,31 @@
+package _07_IFrameAndWindows;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+
+public class _04_Scrollintro extends BaseDriver {
+
+    @Test
+    public void Test01() {
+
+        driver.get("http://selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+
+        // Actions actions = new Actions(driver);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver; // casting
+        js.executeScript("window.scrollTo(0, 500);");
+
+        // js.executeScript("window.scrollBy(0, 500);");   -> from where is belonged dragging
+        // js.executeScript("window.scrollTo(0, 500);");   -> drag till where we wrote
+        // js.executeScript("window.scrollTo(0, document.body.scrollHeight);");   -> drag on the bottom
+        // js.executeScript("arguments[0].scrollIntoView(true);", element);   -> drag till the element
+
+        // element.click -> normal click
+        // action.click -> action click
+
+        WaitAndQuit();
+    }
+}
