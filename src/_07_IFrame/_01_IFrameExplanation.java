0e0cfac4fabb93007c3bff3fa3207e476ccc97a6
Baha <hakanbahadircelik@gmail.com>
2025-03-01 13:56:04 +0400

    explanation of Iframe

---
+++ src/_07_IFrame/_01_IFrameExplanation.java
@@ -0,0 +1,35 @@
+package _07_IFrame;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _01_IFrameExplanation extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("http://chercher.tech/practice/frames");
+
+        driver.switchTo().frame(0); // with index -> FASTEST
+        driver.switchTo().frame("frame1"); // with id -> WORKING SLOW
+        driver.switchTo().frame("iamframe"); // with name -> FAST
+
+        WebElement frame1 = driver.findElement(By.id("frame1"));
+        driver.switchTo().frame(frame1);
+
+        // index > webelement > name > id     / FASTEST TO SLOWEST
+
+        WebElement txtBox = driver.findElement(By.cssSelector("[id='topic']+input"));
+        txtBox.sendKeys("georgia");
+
+
+        driver.switchTo().parentFrame(); // go one page back
+        driver.switchTo().defaultContent();  // go to beginning
+
+
+
+        WaitAndQuit();
+    }
+}
