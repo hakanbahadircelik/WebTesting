4136d54c28b3f866a4e7bcfd9f3b14f26c78f0cf
Baha <hakanbahadircelik@gmail.com>
2025-03-01 14:01:36 +0400

    Example of Iframe

---
+++ src/_07_IFrame/_02_IFrameExample.java
@@ -0,0 +1,25 @@
+package _07_IFrame;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class _02_IFrameExample extends BaseDriver {
+
+    @Test
+    public void Test01(){
+
+        driver.get("http://chercher.tech/practice/frames");
+
+        driver.switchTo().frame(1);
+
+        WebElement webSelect = driver.findElement(By.xpath("animals"));
+        Select select = new Select(webSelect);
+        select.selectByVisibleText("Avatar");
+
+
+        WaitAndQuit();
+    }
+}
